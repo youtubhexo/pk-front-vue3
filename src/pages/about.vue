db29dd68389b6c13886bd114caa12963f7c93bde
desperado <youtubhexo@gmail.com>
2024-09-11 18:40:03 +0800

    集成自动路由的方案【unplugin-vue-router】

---
+++ src/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>这是关于页面</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>hello-world 今天日期：{{ new Date().toLocaleDateString() }}</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
