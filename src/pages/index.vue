a6f3d660df47d0a056c02c29068b80b11228b765
desperado <youtubhexo@gmail.com>
2024-09-12 19:25:01 +0800

    unplugin-auto-import 自动导入插件引入

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,12 @@
   <div>
     <div class="m-4 shadow-md">hello-world 今天日期：{{ new Date().toLocaleDateString() }}</div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    {{ msg }}
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const msg = ref('desperado blog')
+</script>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
 import UnoCSS from 'unocss/vite'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,7 +15,25 @@ export default defineConfig({
     }),
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      /* options */
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        '@vueuse/core'
+      ]
+    })
   ],
   resolve: {
     alias: {
